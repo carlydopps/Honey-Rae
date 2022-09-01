ce91faff75d2ec20c3e94d36b2ef5784d5525ee9
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-09-01 09:15:55 -0500

    Adding Customer List

---
+++ src/components/customers/CustomerList.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import { Customer } from "./Customer"
+import "./Customers.css"
+
+export const CustomerList = () => {
+    const [customers, updateCustomers] = useState([])
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/users?isStaff=false`)
+                .then(res => res.json())
+                .then((customerArray) => {
+                    updateCustomers(customerArray)
+                })
+        },
+        []
+    )
+
+    return <article className="customers">
+        {
+            customers.map(customer => <Customer key={`customer--${customer.id}`}
+                id={customer.id} 
+                fullName={customer.fullName}
+                address={customer.address} 
+                phoneNumber={customer.phoneNumber} />)
+        }
+    </article>
+}
